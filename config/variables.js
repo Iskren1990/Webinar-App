baad77eef29ddd7e3c829d9ba3a7a08af86ff1b9
Iskren1990 <Iskren.1990@abv.bg>
2020-12-10 17:26:55 +0200

    deploy troubleshooting

--- config/variables.js
+++ config/variables.js
@@ -7,7 +7,7 @@ module.exports = {
     key: process.env.KEY_WORD,
     dbOpt: { useUnifiedTopology: true,  useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false },
     uri: (conf) => `mongodb+srv://${conf.dbuser}:${conf.dbpass}@cluster0.4dqi4.mongodb.net/${conf.dbname}?retryWrites=true&w=majority`,
-    // origin: ['http://localhost:5555', 'http://localhost:4200']
+    origin: "*" //['http://localhost:5555', 'http://localhost:4200']
 },
 development: {
     PORT:process.env.PORT,
@@ -17,6 +17,6 @@ development: {
     key: process.env.KEY_WORD,
     dbOpt: { useUnifiedTopology: true,  useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false },
     uri: (conf) => `mongodb+srv://${conf.dbuser}:${conf.dbpass}@cluster0.4dqi4.mongodb.net/${conf.dbname}?retryWrites=true&w=majority`,
-    // origin: ['http://localhost:5555', 'http://localhost:4200']
+    origin: "*" //['http://localhost:5555', 'http://localhost:4200']
 }
 }[process.env.NODE_ENV];
