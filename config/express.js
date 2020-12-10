9b67c97f30aee2ce8613fe6ab43c09e6b74e40f9
Iskren1990 <Iskren.1990@abv.bg>
2020-12-10 17:46:46 +0200

    deploy troubleshooting

--- config/express.js
+++ config/express.js
@@ -5,16 +5,16 @@ const cors = require("cors");
 
 function expressConf(app, options) {
 
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-    app.use(CP());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(CP());
+  console.log(options.origin)
+  app.options("*", cors({
+    origin: options.origin,
+    credentials: true
+  }));
 
-    app.options("*", cors({
-        origin: options.origin,
-        credentials: true
-      }));
-
-    app.use(express.static("public"));
+  app.use(express.static("public"));
 }
 
 module.exports = expressConf;
